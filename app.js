9866a91f91006490bb99466fc5f493810b08df40
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-08-12 00:02:16 +0530

    database relations

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const bodyParser = require("body-parser");
 const _ = require("lodash");
 const session = require("express-session");
 const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require("mongoose");
 
-const Post = require(__dirname + "/db/post");
 const date = require(__dirname + "/utils/date");
-
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -25,8 +25,27 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require(__dirname + "/db/mongooseConnect");
+// const Post = require(__dirname + "/db/post");
+// const User = require(__dirname + "/db/user");
 
-const User = require(__dirname + "/db/user");
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    userId: String
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    dateCreated: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.model('User', userSchema);
+
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
@@ -42,37 +61,37 @@ app.get("/", function (req, res) {
 
 });
 
-app.post("/register", function(req, res){
+app.post("/register", function (req, res) {
 
     User.register(
-        {username: req.body.username, email: req.body.email, dateCreated: date.getDate()},
+        { username: req.body.username, email: req.body.email, dateCreated: date.getDate() },
         req.body.password,
-        function(err, user){
-            if(err){
+        function (err, user) {
+            if (err) {
                 console.log(err);
                 res.redirect("/");
-            }else{
-                passport.authenticate("local")(req, res, function(){
+            } else {
+                passport.authenticate("local")(req, res, function () {
                     res.redirect("/home");
                 });
             }
         });
 
 });
- 
-app.post("/login", function(req, res){
+
+app.post("/login", function (req, res) {
 
     const user = new User({
         username: req.body.username,
         password: req.body.password
     });
 
-    req.login(user, function(err){
+    req.login(user, function (err) {
 
-        if(err){
+        if (err) {
             console.log(err);
-        }else{
-            passport.authenticate("local")(req, res, function(){
+        } else {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/home");
             });
         }
@@ -81,12 +100,22 @@ app.post("/login", function(req, res){
 
 });
 
-app.get("/home", function(req, res){
+app.get("/home", function (req, res) {
 
-    if(req.isAuthenticated()){
-        res.render("home");
+    if (req.isAuthenticated()) {
+
+        Post.find(function (err, posts) {
+
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("home", { allPosts: posts });
+            }
+
+        });
+
         console.log("Serving home page...");
-    }else{
+    } else {
         res.redirect("/");
     }
 
@@ -102,9 +131,9 @@ app.get("/signup", function (req, res) {
 
 app.get("/about", function (req, res) {
 
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         res.render("about");
-    }else{
+    } else {
         res.redirect("/");
     }
 
@@ -112,9 +141,9 @@ app.get("/about", function (req, res) {
 
 app.get("/contact", function (req, res) {
 
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         res.render("contact");
-    }else{
+    } else {
         res.redirect("/");
     }
 
@@ -122,18 +151,30 @@ app.get("/contact", function (req, res) {
 
 app.get("/compose", function (req, res) {
 
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         res.render("compose");
-    }else{
+    } else {
         res.redirect("/");
     }
 });
 
 app.get("/profile", function (req, res) {
 
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         res.render("profile");
-    }else{
+    } else {
+        res.redirect("/");
+    }
+
+});
+
+app.get("/submit", function (req, res) {
+
+    if (req.isAuthenticated()) {
+
+        res.render("submit");
+
+    } else {
         res.redirect("/");
     }
 
@@ -141,60 +182,28 @@ app.get("/profile", function (req, res) {
 
 app.post("/compose", function (req, res) {
 
-    let postTitle = req.body.titleGiven;
-    let postContent = req.body.contentGiven;
-    let postDate = date.getDate();
+    const postTitle = req.body.titleGiven;
+    const postContent = req.body.contentGiven;
 
-    const post = new Post({
+    User.findById(req.user.id, function (err, foundUser) {
 
-        title: postTitle,
-        content: postContent,
-        dateAdded: postDate
-
-    });
-
-    post.save();
-    // const post = {
-    //     title: postTitle,
-    //     content: postContent
-    // }
-    // posts.push(post);
-    // console.log(posts);
-    res.redirect("/home");
-
-});
-
-app.get("/posts/postTitle=:postTitle&postID=:postID", function (req, res) {
-
-    const requestedID = _.lowerCase(req.params.postID);
-
-    Post.find(function(err, posts){
-
-        if(err){
+        if (err) {
             console.log(err);
-        }else{
+        } else {
 
-            posts.forEach(function (post) {
-
-                const storedID = _.lowerCase(post._id);
-        
-                if (storedID === requestedID) {
-                    res.render("post",
-        
-                        {
-        
-                            postTitle: post.title,
-                            postContent: post.content
-        
-                        }
-        
-                    );
-        
-                } else {
-                    //console.log("Not Found!");
-                }
-        
-            });
+            if (foundUser) {
+                const newPost = Post({
+                    title: postTitle,
+                    content: postContent,
+                    userId: foundUser._id
+                });
+                console.log(foundUser._id);
+                newPost.save(function () {
+                    res.redirect("/home");
+                });
+            } else {
+                console.log("user not found!");
+            }
 
         }
 
@@ -202,10 +211,67 @@ app.get("/posts/postTitle=:postTitle&postID=:postID", function (req, res) {
 
 });
 
-app.get("/logout", function(req, res){
+// app.post("/compose", function (req, res) {
 
-    req.logout(function(err){
-        if(err){
+//     let postTitle = req.body.titleGiven;
+//     let postContent = req.body.contentGiven;
+//     let postDate = date.getDate();
+
+//     const post = new Post({
+
+//         title: postTitle,
+//         content: postContent,
+//         dateAdded: postDate
+
+//     });
+
+//     post.save();
+//     // const post = {
+//     //     title: postTitle,
+//     //     content: postContent
+//     // }
+//     // posts.push(post);
+//     // console.log(posts);
+//     res.redirect("/home");
+
+// });
+
+app.get("/posts/postTitle=:postTitle&postID=:postID", function (req, res) {
+
+    const requestedID = _.lowerCase(req.params.postID);
+
+    if (req.isAuthenticated()) {
+
+        Post.find(function (err, posts) {
+            if (err) {
+                console.log(err);
+            } else {
+                posts.forEach(function (post) {
+                    const storedID = _.lowerCase(post._id);
+                    if (storedID === requestedID) {
+                        res.render("post",
+                            {
+                                postTitle: post.title,
+                                postContent: post.content
+                            }
+                        );
+                    } else {
+                        //console.log("Not Found!");
+                    }
+                });
+            }
+        });
+
+    } else {
+        res.redirect("/");
+    }
+
+});
+
+app.get("/logout", function (req, res) {
+
+    req.logout(function (err) {
+        if (err) {
             return next(err);
         }
         res.redirect("/");
