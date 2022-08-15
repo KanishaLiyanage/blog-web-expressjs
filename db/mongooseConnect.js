3ccf625e315b3959a92f0fae73507a50ba6bb9f2
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-08-15 16:44:21 +0530

    heroku deployed

--- db/mongooseConnect.js
+++ db/mongooseConnect.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
-const localUrl = "mongodb://127.0.0.1:27017/blogDB";
+//const localUrl = "mongodb://127.0.0.1:27017/blogDB";
+//const atlasUrl = "mongodb+srv://KanishaL:kanisha123@cluster0.vfqegvn.mongodb.net/blogDB?retryWrites=true&w=majority";
 
-mongoose.connect(localUrl, {useNewUrlParser: true});
+const atlasUrl = process.env.ATLAS_URL;
+mongoose.connect(atlasUrl, {useNewUrlParser: true});
